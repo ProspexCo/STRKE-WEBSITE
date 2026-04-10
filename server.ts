7136e62af1bd5a1d2101b3137806e208558f5972
ProspexCo <prospex46@gmail.com>
2026-04-09 21:21:45 -0400

    Fix CORS preflight

--- server.ts
+++ server.ts
@@ -46,6 +46,13 @@ app.use(
   })
 );
 
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(200);
+});
+
 app.get("/", (_req, res) => {
   res.status(200).json({
     ok: true,
